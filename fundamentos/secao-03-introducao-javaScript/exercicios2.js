cfdbc5c11faecc4ade8d5535d84f82b44a78d0c0
paulotimoteo33 <pauloadrian20@gmail.com>
2023-05-08 19:57:54 -0300

    inicio exercicios6

--- fundamentos/secao-03-introducao-javaScript/exercicios2.js
+++ fundamentos/secao-03-introducao-javaScript/exercicios2.js
@@ -1,7 +1,9 @@
 let numero1 = 13
 let numero2 = 7
 
-if (numero1 > numero2){
-console.log('O numero maio é ' + numero1)
-}else if(numero1 < numero2){
-console.log('O numero maior é ' + numero2);}else (console.log('numeros iguais'))
+
+if (numero1 > numero2 ){
+console.log('O numero maior é ' + numero1)
+}else if(numero2 > numero1){
+console.log('O numero maior é ' + numero2);}
+else (console.log('numeros são iguais'))
--- fundamentos/secao-03-introducao-javaScript/exercicios3.js
+++ fundamentos/secao-03-introducao-javaScript/exercicios3.js
@@ -3,7 +3,7 @@ let numero2 = 7
 let numero3 = 20
 
 if (numero1 > numero2 && numero1 > numero3){
-console.log('O numero maio é ' + numero1)
+console.log('O numero maior é ' + numero1)
 }else if(numero2 > numero1 && numero2 > numero3){
 console.log('O numero maior é ' + numero2);}
 else if(numero3 > numero2 && numero3 > numero1){
