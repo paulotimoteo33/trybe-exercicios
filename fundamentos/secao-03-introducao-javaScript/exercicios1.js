1c83020b8f8c18f5dd453efa947c05b33e4a7ee8
paulotimoteo33 <pauloadrian20@gmail.com>
2023-05-08 18:54:50 -0300

    exercicio2

---
+++ fundamentos/secao-03-introducao-javaScript/exercicios1.js
@@ -0,0 +1,7 @@
+let numero1 = 13
+let numero2 = 7
+
+if (numero1 > numero2){
+console.log('O numero maio é ' + numero1)
+}else if(numero1 < numero2){
+console.log('O numero maior é ' + numero2);}else (console.log('numeros iguais'))
