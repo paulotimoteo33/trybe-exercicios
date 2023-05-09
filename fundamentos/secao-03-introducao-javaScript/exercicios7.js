351d506000e0de6fb769a7659965c36f7001fa5c
paulotimoteo33 <pauloadrian20@gmail.com>
2023-05-09 11:53:01 -0300

    exercicio7

---
+++ fundamentos/secao-03-introducao-javaScript/exercicios7.js
@@ -0,0 +1,25 @@
+
+
+
+let nota = 93
+
+if (nota >= 90 && nota <= 100 ){
+console.log('nota A')
+}
+else if (nota >= 80 && nota < 90){
+console.log('nota B')
+}
+else if (nota >= 70 && nota < 80){
+    console.log('nota C')
+}
+else if (nota >= 60 && nota < 70){
+    console.log('nota D')
+}
+else if (nota >= 50 && nota < 60){
+    console.log('nota E')
+    }
+else if (nota < 50 && nota > 0){
+    console.log('nota F')
+    }
+    else if (nota > 100 || nota < 0){console.log('Erro')}
+    
